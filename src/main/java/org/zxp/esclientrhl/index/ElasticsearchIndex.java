5c24c655a4c723f2f16f2e4ae2b6475334bc0671
xuwencheng <xuwencheng@meituan.com>
2020-09-20 15:43:17 +0800

    first commit

---
+++ src/main/java/org/zxp/esclientrhl/index/ElasticsearchIndex.java
@@ -0,0 +1,63 @@
+package org.zxp.esclientrhl.index;
+
+import java.util.Map;
+
+/**
+ * program: esdemo
+ * description: 索引结构基础方法接口
+ * author: X-Pacific zhang
+ * create: 2019-01-25 16:52
+ **/
+public interface ElasticsearchIndex<T> {
+    /**
+     * 创建索引
+     * @param clazz
+     * @throws Exception
+     */
+    public void createIndex(Class<T> clazz) throws Exception;
+
+    /**
+     * 切换Alias写入index
+     * @param clazz
+     * @throws Exception
+     */
+    public void switchAliasWriteIndex(Class<T> clazz,String writeIndex) throws Exception;
+
+
+    /**
+     * 创建Alias
+     * @param clazz
+     * @throws Exception
+     */
+    public void createAlias(Class<T> clazz) throws Exception;
+
+    /**
+     * 创建索引
+     * @param settings
+     * @param mappingJson
+     * @throws Exception
+     */
+    public void createIndex(Map<String,String> settings, Map<String,String[]> settingsList, String mappingJson, String indexName,String indexType) throws Exception;
+
+    /**
+     * 删除索引
+     * @param clazz
+     * @throws Exception
+     */
+    public void dropIndex(Class<T> clazz) throws Exception;
+
+    /**
+     * 索引是否存在
+     * @param clazz
+     * @throws Exception
+     */
+    public boolean exists(Class<T> clazz) throws Exception;
+
+    /**
+     * 滚动索引
+     * @param clazz
+     * @param isAsyn 是否异步
+     * @throws Exception
+     */
+    public void rollover(Class<T> clazz,boolean isAsyn) throws Exception;
+}
